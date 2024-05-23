70c384d563248bf1ce5dd8b1a41363d2a13b8070
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-23 12:25:18 +0330

    [todo] todos for login and resgier functions

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -2,6 +2,13 @@ import { BaseMiddleware } from "../middlewares/intypes";
 import { jsonRes } from "../utils/helper";
 
 export const login: BaseMiddleware = (req, res, next) => {
+  // check user email is exists or not
+  // check password is currect
+  // create jwt token and send it to user
   jsonRes(res, "Login");
 };
-export const signup: BaseMiddleware = (req, res, next) => {};
+export const signup: BaseMiddleware = (req, res, next) => {
+  // check email is exists or not, if exists then register process failed
+  // make password hash
+  // return message that user created
+};
