58fc0b3667efc648cee7f969fe12de03398793fd
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-23 12:40:11 +0330

    [dev] add register validation to /signup route

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { login, signup } from "../controllers/authController";
-import { LoginValidation } from "../middlewares/validationMiddleware";
+import {
+  LoginValidation,
+  ResgierValidation,
+} from "../middlewares/validationMiddleware";
 
 const authRouter = Router();
 
 authRouter.post("/login", LoginValidation, login);
-authRouter.post("/signup", signup);
+authRouter.post("/signup", ResgierValidation, signup);
 
 export default authRouter;
