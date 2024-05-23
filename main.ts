ab5efc2ac05220b5782b4cfecdfe9007f70fa67a
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-23 18:05:11 +0330

    [fix] register cors

--- main.ts
+++ main.ts
@@ -3,11 +3,13 @@ import http from "http";
 import env from "./utils/env";
 import connectToDB from "./utils/db";
 import router from "./routes";
+import cors from "cors";
 
 async function main() {
   const app = express();
   const server = http.createServer(app);
 
+  app.use(cors());
   app.use(express.json());
   // main router
   app.use(router);
