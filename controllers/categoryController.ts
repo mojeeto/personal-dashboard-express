5461e666abd2da31ab82f33a6605b0ebf7c4c453
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-25 15:09:12 +0330

    [feat] create category

--- controllers/categoryController.ts
+++ controllers/categoryController.ts
@@ -1,9 +1,17 @@
 import { BaseMiddleware } from "../middlewares/intypes";
-import { fetchAllCategories } from "../repo/categoryRepo";
+import { createCategory, fetchAllCategories } from "../repo/categoryRepo";
 import { jsonRes } from "../utils/helper";
 
 export const getCategories: BaseMiddleware = async (req, res, next) => {
   const categories = await fetchAllCategories();
   jsonRes(res, "Get All Categories", { data: categories });
 };
-export const postCategory: BaseMiddleware = (req, res, next) => {};
+export const postCategory: BaseMiddleware = async (req, res, next) => {
+  const { title } = req.body;
+  try {
+    await createCategory(title, req.userId!);
+    jsonRes(res, "Category created!");
+  } catch (err) {
+    jsonRes(res, "Something went wronge while create new category!");
+  }
+};
--- repo/categoryRepo.ts
+++ repo/categoryRepo.ts
@@ -3,3 +3,8 @@ import categoryModel from "../models/catergoryModel";
 export async function fetchAllCategories() {
   return await categoryModel.find().exec();
 }
+
+export async function createCategory(title: string, userId: string) {
+  const category = new categoryModel({ title, user_id: userId });
+  return await category.save();
+}
