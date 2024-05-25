f3c464dbe2eac9344adb2cf0e30acd4c53456731
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-25 17:55:19 +0330

    [fix] add currency to authorizedRoutes

--- routes/authorizedRoutes.ts
+++ routes/authorizedRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import categoryRouter from "./categoryRoutes";
 import { isAuthenticated } from "../middlewares/validationMiddleware";
+import currencyRouter from "./currencyRoutes";
 
 const authorizedRoutes = Router();
 
 // middleware for isAuthenticated Middleware
 authorizedRoutes.use(isAuthenticated);
 authorizedRoutes.use(categoryRouter);
+authorizedRoutes.use(currencyRouter);
 
 export default authorizedRoutes;
