ff56a839ada14735b546afc349c9cc9a050febbd
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-27 15:02:03 +0330

    [feat] RecordType for wallet

--- models/walletModel.ts
+++ models/walletModel.ts
@@ -1,20 +1,27 @@
 import { Document, PopulatedDoc, model, Schema } from "mongoose";
 import { ICategory } from "./catergoryModel";
 import { IContact } from "./contactModel";
-import { ZodIssueCode, z } from "zod";
+import { z } from "zod";
 
 /*
  * Wallet Model
  * title::string that represent to what you buy
  * price::the price of that thing
+ * type::represent that money spended or income from someone
  * category_id::that point to which category you buy
  *            ::Like Food & Drink
  * contact_id::that point to from who or place you buy it
  * for::that point to for whos you buy that
  * */
+export enum TWalletTypeRecord {
+  expend = "EXPEND",
+  income = "INCOME",
+}
+
 export type TWallet = {
   title: string;
   price: number;
+  recordType: TWalletTypeRecord;
   category_id: PopulatedDoc<ICategory & Document>;
   contact_id: PopulatedDoc<IContact & Document>;
   forContacts: PopulatedDoc<IContact & Document>[];
@@ -28,6 +35,9 @@ export const walletZodValidation = z.object({
   }),
   price: z.number({
     required_error: "Price is required!",
+  }),
+  recordType: z.enum(["EXPEND", "INCOME"], {
+    required_error: "Record Type must be Expend or Income string.",
   }),
   category_id: z.string({
     required_error: "Category is required!",
@@ -50,6 +60,11 @@ const WalletSchema = new Schema<IWallet>(
     },
     price: {
       type: Number,
+      required: true,
+    },
+    recordType: {
+      type: String,
+      enum: TWalletTypeRecord,
       required: true,
     },
     category_id: {
